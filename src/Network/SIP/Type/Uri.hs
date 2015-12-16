3f130e26023c24debd44725ef0d527fdee4f1bdb
Jan Sipr <sipr.jan@gmail.com>
2015-12-16 13:40:37 +0100

    Add URI type

---
+++ src/Network/SIP/Type/Uri.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module:       Network.SIP.Type.URI
+-- Description:  Sip uri type
+-- Copyright:    Copyright (c) 2015 Jan Sipr
+-- License:      MIT
+--
+-- Uri from from Network.URI is not able correctly parser sip:name@host format.
+-- This data type and belonging parser is here to fill the gap.
+module Network.SIP.Type.Uri
+    ( Uri(..)
+    , Scheme(..)
+    )
+  where
+
+import Data.Data (Data)
+import Data.Eq (Eq)
+import Data.Maybe (Maybe)
+import Data.Ord (Ord)
+import Data.Text (Text)
+import Data.Typeable (Typeable)
+import Data.Word (Word16)
+import GHC.Generics (Generic)
+import Text.Show (Show)
+
+data Scheme = SIP | SIPS
+  deriving (Show, Eq, Data, Ord, Generic, Typeable)
+
+data Uri = Uri
+    { uriScheme :: Scheme
+    , uriUser :: Maybe Text
+    , uriHost :: Text
+    , uriPort :: Maybe Word16
+    }
+  deriving (Show, Eq, Data, Generic, Typeable)
