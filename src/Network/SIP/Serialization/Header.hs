eec3ce73767a65470a97117fa5827e0747c8c3eb
Jan Sipr <sipr.jan@gmail.com>
2016-01-23 13:18:47 +0100

    Header serialization and tests

---
+++ src/Network/SIP/Serialization/Header.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module:       Network.SIP.Serialization.Header
+-- Description:  SIP message serialization.
+-- Copyright:    Copyright (c) 2015 Jan Sipr
+-- License:      MIT
+--
+-- Serialize SIP message int to transportable form.
+module Network.SIP.Serialization.Header
+    ( serializeHeader
+    )
+  where
+
+import Data.ByteString (ByteString)
+import Data.CaseInsensitive (original)
+import Data.Function (($), (.))
+import Data.List (lookup)
+import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
+import Data.Text.Encoding (encodeUtf8)
+
+import Network.SIP.Type.Header
+    ( Header
+    , HeaderName (Custom)
+    , headerNameMap
+    )
+
+serializeHeader :: Header -> ByteString
+serializeHeader (Custom t, v) = encodeUtf8 $ "X-" <> t <> ": " <> v
+serializeHeader (h, v) =
+    -- TODO: Function fromJust in this section is not good, it needs to be
+    -- rewritten and some sort of exception handling must be "invented".
+    (original . fromJust $ lookup h headerNameMap) <> ": " <>
+    (encodeUtf8 v)
---
+++ test/TestCase/Network/SIP/Serialization/Header.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module:       TestCase.Network.SIP.Serialization.Header
+-- Description:  SIP message serialization test
+-- Copyright:    Copyright (c) 2015 Jan Sipr
+-- License:      MIT
+module TestCase.Network.SIP.Serialization.Header (tests)
+  where
+
+import Data.ByteString (ByteString)
+import Data.Monoid ((<>))
+import Data.Function (($))
+import Data.Text.Encoding (decodeUtf8)
+
+import Test.Framework (Test, testGroup)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit.Base ((@=?))
+import Test.HUnit.Lang (Assertion)
+
+import Network.SIP.Type.Header
+    ( HeaderName
+        ( Accept
+        , AcceptEncoding
+        , Custom
+        )
+    )
+import Network.SIP.Serialization.Header (serializeHeader)
+
+testHeader :: HeaderName -> ByteString -> ByteString-> Assertion
+testHeader headerName headerName' value =
+    headerName' <> ": " <> value @=?
+        serializeHeader (headerName, decodeUtf8 value)
+
+tests :: [Test]
+tests =
+    [ testGroup "Header serialization tests"
+        [ testCase
+            "Simple header"
+                $ testHeader Accept "Accept" "value"
+        , testCase
+            "Header with dash"
+                $ testHeader AcceptEncoding "Accept-Encoding" "value2 val val"
+        , testCase
+            "Custom header"
+                $ testHeader (Custom "Custom") "X-Custom" "foo bar baz"
+        ]
+    ]
